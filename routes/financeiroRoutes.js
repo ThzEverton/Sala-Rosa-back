d14d3f8851ab2e173e20662f8ef1207838c5c0a1
ThzEverton <evertonthomazdelimasilva070@gmail.com>
2026-05-11 17:45:59 -0300

    fix: stabilize WhatsApp session and permissions

--- routes/financeiroRoutes.js
+++ routes/financeiroRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 let ctrl = new FinanceiroController();
 let auth = new AuthMiddleware();
 
-router.get("/", auth.validarToken, (req, res) => {
+router.get("/", auth.validarToken, auth.somenteGerente, (req, res) => {
 
     // #swagger.tags = ['Financeiro']
     // #swagger.summary = 'Lista lançamentos financeiros com filtros opcionais (?status=PAGO&inicio=2024-01-01&fim=2024-12-31)'
